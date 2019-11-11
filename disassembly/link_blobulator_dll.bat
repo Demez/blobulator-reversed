ee168ab70b1bb7c96ba54ac90a0e9c9d10662cb2
ethan101au <ethanharris147@gmail.com>
2019-11-11 10:14:37 +1000

    Added and enforced .gitignore

--- disassembly/link_blobulator_dll.bat
+++ disassembly/link_blobulator_dll.bat
@@ -6,6 +6,7 @@ set files=%_path%\blobulator_obj\*.obj %_path%\..\..\..\lib\public\tier0.lib %_p
 cd %VS140COMNTOOLS%
 call vsvars32.bat
 
+if not exist %_path%\bin
 mkdir %_path%\bin
 link.exe /dll %files% /debug /out:%_path%\bin\blobulator.dll
 
--- disassembly/link_blobulator_lib.bat
+++ disassembly/link_blobulator_lib.bat
@@ -5,7 +5,9 @@ set _path="%cd%"
 cd %VS140COMNTOOLS%
 call vsvars32.bat
 
+if not exist %_path%\bin
 mkdir %_path%\bin
+
 link.exe /lib %_path%/blobulator_obj/*.obj /out:%_path%\bin\blobulator.lib
 
 pause
