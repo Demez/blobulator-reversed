056dc6008ac12eb852b77fed7264e3c9ac590169
ethan101au <ethanharris147@gmail.com>
2019-11-11 10:18:50 +1000

    Fixed blobulator lib and dll linking scripts

--- disassembly/link_blobulator_dll.bat
+++ disassembly/link_blobulator_dll.bat
@@ -1,13 +1,15 @@
 @echo off
 
 set _path="%cd%"
-set files=%_path%\blobulator_obj\*.obj %_path%\..\..\..\lib\public\tier0.lib %_path%\..\..\..\lib\public\tier2.lib %_path%\..\..\..\lib\public\mathlib.lib
+set files=%_path%\blobulator_obj\*.obj %_path%\lib\tier0.lib %_path%\lib\tier2.lib %_path%\lib\mathlib.lib
 
 cd %VS140COMNTOOLS%
 call vsvars32.bat
 
-if not exist %_path%\bin
+if not exist %_path%\bin (
 mkdir %_path%\bin
+)
+
 link.exe /dll %files% /debug /out:%_path%\bin\blobulator.dll
 
 pause
--- disassembly/link_blobulator_lib.bat
+++ disassembly/link_blobulator_lib.bat
@@ -5,8 +5,9 @@ set _path="%cd%"
 cd %VS140COMNTOOLS%
 call vsvars32.bat
 
-if not exist %_path%\bin
+if not exist %_path%\bin (
 mkdir %_path%\bin
+)
 
 link.exe /lib %_path%/blobulator_obj/*.obj /out:%_path%\bin\blobulator.lib
 
